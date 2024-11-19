e847be357873087508ce5f689458dbb4d5715611
Ian Simons <iansimons93@gmail.com>
2024-11-19 14:09:54 -0500

    creating main function

---
+++ project.ps1
@@ -0,0 +1,8 @@
+
+function Main(){
+	write-host "Welcome! please select an option below"
+	write-host "1) define user inactivity period"
+	write-host "2) fetch inactive users"
+	write-host "3) change all inactive users"
+	write-host "4) change individual inactive users"
+	$choice = read-host "please enter your choice"

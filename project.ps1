9a7f7f3c3b65bcfcca2c35cf6ec06ba21cb36a6d
Ki Hoon <kihoon.kim38@outlook.com>
2024-11-19 14:20:05 -0500

    Menu update project.ps1
    Created Menu as a function to clean up code.

--- project.ps1
+++ project.ps1
@@ -1,8 +1,14 @@
-
-function Main(){
+function Print-Menu{
+	#Clears user screen for a clean look of the menu
+	Clear-Host 
 	write-host "Welcome! please select an option below"
 	write-host "1) define user inactivity period"
 	write-host "2) fetch inactive users"
 	write-host "3) change all inactive users"
 	write-host "4) change individual inactive users"
 	$choice = read-host "please enter your choice"
+}
+
+function Main(){
+	Print-Menu
+ }

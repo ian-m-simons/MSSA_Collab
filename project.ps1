4e303e08f06ff265bdf1a6417d811c98bdf227f3
Ki Hoon <kihoon.kim38@outlook.com>
2024-11-20 11:03:21 -0500

    Incorporated Error Handling and Option 1

--- project.ps1
+++ project.ps1
@@ -1,28 +1,10 @@
 # Variable for number of days considered Threshold
-$InactiveThreshold = 60
-
-function inputInt(){
-        param ([string]$prompt)
-
-        $choice = ""
-        $success = $false
-        while ($success -eq $false){
-                $choice = read-host($prompt)
-                try{
-                        $choice = [int]$choice
-                        $success = $true
-                }
-                catch{
-                        write-host "[ERROR] Input must be integer only"
-                }
-        }
-        return $choice
-}
-
-
-function Print-Menu{
+$global:inactiveDays = ""
+$global:inactiveUsers = ""
+$global:inactiveThreshold = ""
+function Get-Menu{
 	#Clears user screen for a clean look of the menu
-	Clear-Host 
+	
 	write-host "Welcome! please select an option below"
 	write-host "1) define user inactivity period"
 	write-host "2) fetch inactive users"
@@ -34,11 +16,24 @@ function Print-Menu{
 #Option 1 Function - Josh
 function Set-InactivePeriod{
 	#Paste code here#
+    $global:inactiveDays = inputint "Enter number of days for inactivity threshold"
+ 	$global:inactiveThreshold = (Get-Date).AddDays(-$inactiveDays)
+    
+    # Retrieves all users in domain I believe? 
+	$users = Get-ADUser -Filter * -Properties LastLogonDate
+
+
+	# users who have not logged on since inactive date 
+	$global:inactiveUsers = $users | Where-Object { $_.LastLogonDate -lt $inactiveThreshold -or  !$_.LastLogonDate }
+
+	# I think this should show the innactive users by name/ account name.
+	
 }
 
 #Option 2 Function - Brittney
 function Get-InactiveUsers{
 	#Paste code here#
+    $inactiveUsers | Select-Object Name, SamAccountName, LastLogonDate | Format-Table -AutoSize
 }
 
 #Option 3 Function - 
@@ -51,29 +46,71 @@ function Set-InactiveUser{
 	#Paste code here#
 }
 
+#Input Validations below - Ian
+# Input Validation - Number of days for Inactive Status
+function inputInt(){
+	param ([string]$prompt)
+	
+	$choice = ""
+	$success = $false
+	while ($success -eq $false){
+		$choice = read-host($prompt)
+		try{
+			$choice = [int]$choice
+			$success = $true
+		}
+		catch{
+			write-host "[ERROR] Input must be integer only"
+		}
+	}
+	return $choice
+}
+
+# Input Validation - UserName
+function UserNameValidation(){
+	param([array]$Users, [string]$individual)
+	
+	$userFound = $false
+	for ($i = 0; $i -lt $Users.Length; $i++){
+		if ($individual -eq $Users[$i].UserPrincipalName()){
+			$userFound = $true
+		}
+		else{
+			continue
+		}
+	}
+	if ($userFound){
+		return $true
+	}
+	else{
+		return $false
+	}
+}
+
+# Main method area starts here
 do {
-    Print-Menu
-    $choice = inputInt("please enter your choice")
-    
+    Get-Menu
+    $choice = inputINt "Please Select a number: "   
     switch ($choice) {
         '1' { # Josh 
-            Write-Host "Option One"
-	    Set-InactivePeriod
-
+            Write-Host "`nOption One"
+            Set-InactivePeriod
+            Write-Host "`nThe inactivity threshold has been set to $inactiveDays days"
+            Write-Host "Last logon prior to: `n$inactiveThreshold`n"
         }
         '2' { # Brittney
             Write-Host " Option Two"
-	    Get-InactiveUsers
+	        Get-InactiveUsers
   
         }
         '3' {
             Write-Host " Option Three"
-	    Set-AllInactiveUsers
+	        Set-AllInactiveUsers
 
         }
         '4' { #Luke
             Write-Host " Option Four"
-	    Set-InactiveUser
+	        Set-InactiveUser
 
         }
         '0' {
